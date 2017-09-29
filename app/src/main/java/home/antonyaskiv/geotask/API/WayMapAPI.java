a566c9833e5f6ab850461a5dd06a7cc189ecc20d
Yaskiv Anton <yaskiv22@gmail.com>
2017-09-29 13:37:53 +0200

    Model & Retrofit & Api

---
+++ app/src/main/java/home/antonyaskiv/geotask/API/WayMapAPI.java
@@ -0,0 +1,18 @@
+package home.antonyaskiv.geotask.API;
+
+import android.database.Observable;
+
+import home.antonyaskiv.geotask.Model.WayPoints;
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+/**
+ * Created by AntonYaskiv on 29.09.2017.
+ */
+
+public interface WayMapAPI {
+    @GET("maps/api/directions/json?")
+    Call<WayPoints> getWayFromCoordinate(@Query("origin")String from, @Query("destination") String to, @Query("key") String api_key);
+
+}
---
+++ app/src/main/java/home/antonyaskiv/geotask/Model/Step.java
@@ -0,0 +1,87 @@
+
+package home.antonyaskiv.geotask.Model;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class Step {
+
+    @SerializedName("distance")
+    @Expose
+    private Distance_ distance;
+    @SerializedName("duration")
+    @Expose
+    private Duration_ duration;
+    @SerializedName("end_location")
+    @Expose
+    private EndLocation_ endLocation;
+    @SerializedName("html_instructions")
+    @Expose
+    private String htmlInstructions;
+    @SerializedName("polyline")
+    @Expose
+    private Polyline polyline;
+    @SerializedName("start_location")
+    @Expose
+    private StartLocation_ startLocation;
+    @SerializedName("travel_mode")
+    @Expose
+    private String travelMode;
+
+    public Distance_ getDistance() {
+        return distance;
+    }
+
+    public void setDistance(Distance_ distance) {
+        this.distance = distance;
+    }
+
+    public Duration_ getDuration() {
+        return duration;
+    }
+
+    public void setDuration(Duration_ duration) {
+        this.duration = duration;
+    }
+
+    public EndLocation_ getEndLocation() {
+        return endLocation;
+    }
+
+    public void setEndLocation(EndLocation_ endLocation) {
+        this.endLocation = endLocation;
+    }
+
+    public String getHtmlInstructions() {
+        return htmlInstructions;
+    }
+
+    public void setHtmlInstructions(String htmlInstructions) {
+        this.htmlInstructions = htmlInstructions;
+    }
+
+    public Polyline getPolyline() {
+        return polyline;
+    }
+
+    public void setPolyline(Polyline polyline) {
+        this.polyline = polyline;
+    }
+
+    public StartLocation_ getStartLocation() {
+        return startLocation;
+    }
+
+    public void setStartLocation(StartLocation_ startLocation) {
+        this.startLocation = startLocation;
+    }
+
+    public String getTravelMode() {
+        return travelMode;
+    }
+
+    public void setTravelMode(String travelMode) {
+        this.travelMode = travelMode;
+    }
+
+}
